3e5730e02a560b736cf4ba20b23452ef704244ac
David Dobervich <david.dobervich@gmail.com>
2016-10-08 09:53:32 -0700

    first commit

---
+++ src/NumberBagTester.java
@@ -0,0 +1,14 @@
+
+public class NumberBagTester {
+	public static void main(String[] args) {
+		NumberBag b = new NumberBag(100);	// stores numbers from 0 to 100
+		
+		b.add(3);
+		b.add(3);
+		b.add(5);
+		
+		System.out.println("There are: " + b.getFreqFor(3) + " 3's");
+		
+		
+	}
+}
